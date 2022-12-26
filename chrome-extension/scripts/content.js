dede05ade4fbaf76291ba536644d5288ae10f854
Harold Bracy <harold_bracy@alumni.brown.edu>
2022-12-26 13:00:16 -0500

    styling

--- chrome-extension/scripts/content.js
+++ chrome-extension/scripts/content.js
@@ -6,17 +6,18 @@ function addSideBar(summary) {
 
   // Make sidebar
   const div = document.createElement('div');
+  div.className = 'extension-frame';
   div.style.position = 'fixed';
   div.style.right = '0';
   div.style.bottom = '0';
   div.style.width = `${barWidth}px`;
-  div.style.maxHeight = '30vh';
+  div.style.maxHeight = '50vh';
   div.style.zIndex = '10000000';
   div.style.backgroundColor = '#252526';
   div.style.color = 'white';
   div.style.marginRight = `${stdMargin * 2}px`;
   div.style.marginBottom = `${stdMargin * 2}px`;
-  div.style.padding = `${stdMargin}px`;
+  div.style.padding = `${stdMargin * 2}px`;
   div.style.borderWidth = '1px';
   div.style.borderRadius = `${stdMargin * 2}px`;
   div.style.display = 'flex';
@@ -24,11 +25,12 @@ function addSideBar(summary) {
   const ul = document.createElement('ul');
   ul.className = 'extension-scroll';
   ul.style.overflowY = 'scroll';
-
+  ul.style.paddingRight = `${stdMargin}px`;
+  ul.style.margin = '0px';
   const bullets = summary.split('\u2022');
   bullets.slice(1).forEach(bullet => {
     const li = document.createElement('li');
-    li.innerHTML = `${bullet}<br>`;
+    li.innerHTML = `${bullet}<br><br>`;
     ul.appendChild(li);
   });
   div.appendChild(ul);
--- pages/api/summarize.ts
+++ pages/api/summarize.ts
@@ -25,13 +25,15 @@ export default async function handler(
 
   const response: string = (await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `Summarize the following in bullet points: ${body.text}`,
+    // eslint-disable-next-line max-len
+    prompt: `Summarize the following into multiple bullet points using \u2022. Prefer bullet points over semicolons: ${body.text}`,
     temperature: 0.6,
-    max_tokens: 100,
+    max_tokens: 200,
     top_p: 1,
     frequency_penalty: 1,
     presence_penalty: 1,
     n: 1,
   })).data.choices[0].text || '';
+
   res.status(200).json({ summary: response });
 }
