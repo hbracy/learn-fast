931f3d78df14ec01f0cf78c08ed61bb194870d6d
Harold Bracy <harold_bracy@alumni.brown.edu>
2022-12-27 18:48:25 -0500

    shipit

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -26,21 +26,18 @@ chrome.runtime.onInstalled.addListener((reason) => {
   });
 });
 
-const extensions = 'https://developer.chrome.com/docs/extensions';
-const webstore = 'https://developer.chrome.com/docs/webstore';
-
 // When the user clicks on the extension action
 chrome.commands.onCommand.addListener(async (command, tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-    // Set the action badge to the next state
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    });
-  }
+  console.log('PRESSED');
+  // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  // Next state will always be the opposite
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+  // Set the action badge to the next state
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+  chrome.tabs.sendMessage(tab.id, nextState);
+  // chrome.tabs.executeScript(null, { file: 'scripts/content.js' });
 });
--- chrome-extension/scripts/content.js
+++ chrome-extension/scripts/content.js
@@ -1,64 +1,71 @@
-const text = document.body.innerText;
+chrome.runtime.onMessage.addListener((nextState) => {
+  if (nextState === 'ON') {
+    const text = document.body.innerText;
+    // Make sidebar
+    const barWidth = 300;
+    const stdMargin = 10;
+    const div = document.createElement('div');
+    div.className = 'extension-frame';
+    div.style.position = 'fixed';
+    div.style.right = '0';
+    div.style.bottom = '0';
+    div.style.width = `${barWidth}px`;
+    div.style.maxHeight = '50vh';
+    div.style.minHeight = '30vh';
+    div.style.zIndex = '10000000';
+    div.style.backgroundColor = '#252526';
+    div.style.color = 'white';
+    div.style.marginRight = `${stdMargin * 2}px`;
+    div.style.marginBottom = `${stdMargin * 2}px`;
+    div.style.padding = `${stdMargin * 2}px`;
+    div.style.borderWidth = '1px';
+    div.style.borderRadius = `${stdMargin * 2}px`;
+    div.style.display = 'flex';
+    div.innerText = 'Please wait...';
+    const ul = document.createElement('ul');
+    ul.className = 'extension-scroll';
+    ul.style.overflowY = 'scroll';
+    ul.style.paddingRight = `${stdMargin}px`;
+    ul.style.margin = '0px';
+    // Inject to current page
+    document.querySelector('body').prepend(div);
 
-function addSideBar(summary) {
-  const barWidth = 300;
-  const stdMargin = 10;
-
-  // Make sidebar
-  const div = document.createElement('div');
-  div.className = 'extension-frame';
-  div.style.position = 'fixed';
-  div.style.right = '0';
-  div.style.bottom = '0';
-  div.style.width = `${barWidth}px`;
-  div.style.maxHeight = '50vh';
-  div.style.zIndex = '10000000';
-  div.style.backgroundColor = '#252526';
-  div.style.color = 'white';
-  div.style.marginRight = `${stdMargin * 2}px`;
-  div.style.marginBottom = `${stdMargin * 2}px`;
-  div.style.padding = `${stdMargin * 2}px`;
-  div.style.borderWidth = '1px';
-  div.style.borderRadius = `${stdMargin * 2}px`;
-  div.style.display = 'flex';
-
-  const ul = document.createElement('ul');
-  ul.className = 'extension-scroll';
-  ul.style.overflowY = 'scroll';
-  ul.style.paddingRight = `${stdMargin}px`;
-  ul.style.margin = '0px';
-  const bullets = summary.split('\u2022');
-  bullets.slice(1).forEach(bullet => {
-    const li = document.createElement('li');
-    li.innerHTML = `${bullet}<br><br>`;
-    ul.appendChild(li);
-  });
-  div.appendChild(ul);
-
-  // Inject to current page
-  document.querySelector('body').prepend(div);
-}
-
-if (text) {
-  fetch('http://localhost:3000/api/summarize', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ text }),
-    mode: 'cors',
-  }).then(
-    (response) => {
-      if (response.status !== 200) {
-        console.error(`Looks like there was a problem. Status Code: ${response.status}`);
-        return;
-      }
-      // Examine the text in the response
-      response.json().then((data) => {
-        addSideBar(data.summary);
+    function addText(summary) {
+      div.innerText = '';
+      const bullets = summary.split('\u2022');
+      bullets.slice(1).forEach(bullet => {
+        const li = document.createElement('li');
+        li.innerHTML = `${bullet}<br><br>`;
+        ul.appendChild(li);
       });
-    },
-  ).catch((err) => {
-    console.error('Fetch Error :-S', err);
-  });
-}
+      div.appendChild(ul);
+    }
+
+    if (text) {
+      fetch('http://localhost:3000/api/summarize', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text }),
+        mode: 'cors',
+      }).then(
+        (response) => {
+          if (response.status !== 200) {
+            console.error(`Looks like there was a problem. Status Code: ${response.status}`);
+            return;
+          }
+          // Examine the text in the response
+          response.json().then((data) => {
+            addText(data.summary);
+          });
+        },
+      ).catch((err) => {
+        console.error('Fetch Error :-S', err);
+      });
+    }
+  } else {
+    const div = document.getElementsByClassName('extension-frame')[0];
+    div.remove();
+  }
+});
